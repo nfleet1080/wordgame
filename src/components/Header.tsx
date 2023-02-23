d7482535ef542d3f4a30b23f8871bd3960183b95
nfleet1080 <nfleet1080@gmail.com>
2023-02-22 22:25:28 -0600

    added school logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import { Container, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import { useAppSelector } from '../hooks';
 
-type Props = {}
+type Props = {};
 
 const Header = (props: Props) => {
-  const wins = useAppSelector((state) => state.game.wins);
-  const losses = useAppSelector((state) => state.game.losses);
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container fluid className="px-4">
-        <Navbar.Brand>Game Name</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="me-4">
-            Wins: <strong className="text-success">{wins}</strong>
-          </Navbar.Text>
-          <Navbar.Text>
-            Losses: <strong className="text-danger">{losses}</strong>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
-}
+	const wins = useAppSelector((state) => state.game.wins);
+	const losses = useAppSelector((state) => state.game.losses);
+	return (
+		<Navbar bg="dark" variant="dark" expand="lg">
+			<Container fluid className="px-4">
+				<Navbar.Brand>
+					<img src="cwscs.png" width="29" height="30" className="d-inline-block align-top me-3" alt="CWSCS logo" />
+					CWSCS Wordle
+				</Navbar.Brand>
+				<Navbar.Toggle />
+				<Navbar.Collapse className="justify-content-end">
+					<Navbar.Text className="me-4">
+						Wins: <strong className="text-success">{wins}</strong>
+					</Navbar.Text>
+					<Navbar.Text>
+						Losses: <strong className="text-danger">{losses}</strong>
+					</Navbar.Text>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
 
-export default Header
+export default Header;
